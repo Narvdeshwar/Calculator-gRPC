cecbdf650be4b2643d9615a2f335206d4737791e
Sam Soni <sam.07soni@gmail.com>
2026-03-27 15:20:13 +0530

    added the server

---
+++ server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	pb "grpc-calculator/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedCalculatorServer
+}
+
+func (s *server) Add(ctx context.Context, req *pb.CalcRequest) (*pb.CalcResponse, error) {
+	result := req.A + req.B
+	return &pb.CalcResponse{
+		Result: result,
+	}, nil
+}
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal("err", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterCalculatorServer(grpcServer, &server{})
+	log.Println("Server started on port 50051")
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatal("err", err)
+	}
+}
